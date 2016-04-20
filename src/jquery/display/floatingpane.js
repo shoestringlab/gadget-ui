4e8954a951a99c883b94b774ad63415a0f06df72
Robert Munn <robert.d.munn@gmail.com>
2016-04-20 10:49:19 -0700

    - change util getStyle to accept a jQuery selector

--- src/jquery/display/floatingpane.js
+++ src/jquery/display/floatingpane.js
@@ -14,7 +14,7 @@ function FloatingPane( selector, options ){
 	this.addCSS();
 
 	// now set height to computed height of control _this has been created
-	this.height = gadgetui.util.getStyle( $( this.selector ).parent()[0], "height" );
+	this.height = gadgetui.util.getStyle( $( this.selector ).parent(), "height" );
 
 	this.relativeOffsetLeft = gadgetui.util.getRelativeParentOffset( this.selector ).left;
 	this.addBindings();
@@ -69,7 +69,7 @@ FloatingPane.prototype.config = function( args ){
 	this.width = ( args.width === undefined ? $( this.selector ).css( "width" ) : args.width );
 	this.minWidth = ( this.title.length > 0 ? Math.max( 100, this.title.length * 10 ) : 100 );
 
-	this.height = ( args.height === undefined ? gadgetui.util.getNumberValue( gadgetui.util.getStyle( $( this.selector )[0], "height" ) ) + ( gadgetui.util.getNumberValue( this.padding ) * 2 ) : args.height );
+	this.height = ( args.height === undefined ? gadgetui.util.getNumberValue( gadgetui.util.getStyle( $( this.selector ), "height" ) ) + ( gadgetui.util.getNumberValue( this.padding ) * 2 ) : args.height );
 	this.interiorWidth = ( args.interiorWidth === undefined ? "": args.interiorWidth );
 	this.opacity = ( ( args.opacity === undefined ? 1 : args.opacity ) );
 	this.zIndex = ( ( args.zIndex === undefined ? 100000 : args.zIndex ) );
