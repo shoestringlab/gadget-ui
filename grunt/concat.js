62a41f08af77dba7ec1f3c50247d2f18ca0c0411
Robert Munn <robertdmunn@gmail.com>
2018-11-25 12:03:11 -0700

    - update for object concat

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,7 @@ module.exports = {
 	javascript : {
 		options: {
 		      sourceMap: true
-		    },	
+		    },
 		src : [ 'src/gadget-ui.js','src/javascript/gadget-ui.model.js','src/polyfill.js',
 		        'src/javascript/display/wrapper/pre.js', 'src/javascript/display/*.js', 'src/javascript/display/wrapper/post.js',
 			   	'src/javascript/input/wrapper/pre.js', 'src/javascript/input/*.js', 'src/javascript/input/wrapper/post.js',
@@ -13,26 +13,27 @@ module.exports = {
 	jquery : {
 		options: {
 		      sourceMap: true
-		    },	
+		    },
 		src : [ 'src/gadget-ui.js','src/jquery/jquery.gadget-ui.model.js', 'src/jquery/fn.js','src/polyfill.js',
 		        'src/jquery/display/wrapper/pre.js', 'src/jquery/display/*.js', 'src/jquery/display/wrapper/post.js',
 			    'src/jquery/input/wrapper/pre.js', 'src/jquery/input/*.js', 'src/jquery/input/wrapper/post.js',
+					'src/jquery/objects/wrapper/pre.js', 'src/jquery/objects/*.js', 'src/jquery/objects/wrapper/post.js',
 		        'src/jquery/gadget-ui.util.js' ],
 		dest : 'dist/jquery.<%= pkg.name %>.js'
 	},
 	css : {
 		options: {
 		      sourceMap: true
-		    },	
+		    },
 		src : [ 'src/css/gadget-ui*.css' ],
 		dest : 'dist/gadget-ui.css'
 	},
 	jquerycss : {
 		options: {
 		      sourceMap: true
-		    },	
+		    },
 		src : [ 'src/css/jquery*.css' ],
 		dest : 'dist/jquery.gadget-ui.css'
 	}
-	
+
 };
