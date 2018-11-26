b7aaaafac8bebc46f19731c8f8c328a6877c6028
Robert Munn <robert.d.munn@gmail.com>
2018-11-26 09:21:03 -0700

    - change in grunt rules

--- grunt/concat.js
+++ grunt/concat.js
@@ -6,7 +6,8 @@ module.exports = {
 		    },
 		src : [ 'src/gadget-ui.js','src/javascript/gadget-ui.model.js','src/polyfill.js',
 		        'src/javascript/display/wrapper/pre.js', 'src/javascript/display/*.js', 'src/javascript/display/wrapper/post.js',
-			   	'src/javascript/input/wrapper/pre.js', 'src/javascript/input/*.js', 'src/javascript/input/wrapper/post.js',
+			   		'src/javascript/input/wrapper/pre.js', 'src/javascript/input/*.js', 'src/javascript/input/wrapper/post.js',
+						'src/objects/wrapper/pre.js', 'src/objects/*.js', 'src/objects/wrapper/post.js',
 		        'src/javascript/gadget-ui.util.js' ],
 		dest : 'dist/<%= pkg.name %>.js'
 	},
@@ -16,8 +17,8 @@ module.exports = {
 		    },
 		src : [ 'src/gadget-ui.js','src/jquery/jquery.gadget-ui.model.js', 'src/jquery/fn.js','src/polyfill.js',
 		        'src/jquery/display/wrapper/pre.js', 'src/jquery/display/*.js', 'src/jquery/display/wrapper/post.js',
-			    'src/jquery/input/wrapper/pre.js', 'src/jquery/input/*.js', 'src/jquery/input/wrapper/post.js',
-					'src/jquery/objects/wrapper/pre.js', 'src/jquery/objects/*.js', 'src/jquery/objects/wrapper/post.js',
+			    	'src/jquery/input/wrapper/pre.js', 'src/jquery/input/*.js', 'src/jquery/input/wrapper/post.js',
+						'src/objects/wrapper/pre.js', 'src/objects/*.js', 'src/objects/wrapper/post.js',
 		        'src/jquery/gadget-ui.util.js' ],
 		dest : 'dist/jquery.<%= pkg.name %>.js'
 	},
