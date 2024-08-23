44c780da3d8feff5bf2463ad243d7110dafe2074
Robert Munn <robert@mentalwildfire.com>
2024-08-23 00:59:13 -0700

    9.0.6

--- dist/gadget-ui.js
+++ dist/gadget-ui.js
@@ -4010,27 +4010,4 @@ gadgetui.util = ( function() {
 	};
 }() );
 
-export var gadgetui = gadgetui;
-export var model = gadgetui.model;
-export var display = gadgetui.display;
-export var input = gadgetui.input;
-export var bubble = gadgetui.display.Bubble;
-export var collapsiblepane = gadgetui.display.CollapsiblePane;
-export var dialog = gadgetui.display.Dialog;
-export var fileuploadwrapper = gadgetui.display.FileUploadWrapper;
-export var floatingpane = gadgetui.display.FloatingPane;
-export var menu = gadgetui.display.Menu;
-export var modal = gadgetui.display.Modal;
-export var overlay = gadgetui.display.Overlay;
-export var progressbar = gadgetui.display.ProgressBar;
-export var sidebar = gadgetui.display.Sidebar;
-export var tabs = gadgetui.display.Tabs;
-export var combobox = gadgetui.input.ComboBox;
-export var fileuploader = gadgetui.input.FileUploader;
-export var lookuplistinput = gadgetui.input.LookupListInput;
-export var selectinput = gadgetui.input.SelectInput;
-export var textinput = gadgetui.input.TextInput;
-export var constructor = gadgetui.objects.Constructor;
-export var util = gadgetui.util;
-
 //# sourceMappingURL=gadget-ui.js.map
